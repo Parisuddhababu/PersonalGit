4dbcc620c3fce2029f171cd46a87108aba73473f
Parisuddhababu <parisuddhababu333@gmail.com>
2025-06-28 18:26:47 +0530

    Delete tailwind training practicals/Parisuddha:PSD directory

--- tailwind training practicals/Parisuddha:PSD/src/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    let header = document.querySelector("header");
-    if (window.scrollY > 0) {
-      header.classList.add("active");
-    } else {
-      header.classList.remove("active");
-    }
-  };
